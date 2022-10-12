234cb4b11b2242b056f7ae183b40d22013987da1
Naufal Fadhilla <fadhillanaufal6@gmail.com>
2022-10-11 18:54:05 -0700

    12-10-2022 tugas 17.1 yang pertama

---
+++ latihan17.1.js
@@ -0,0 +1,15 @@
+var menu = 1;
+/* 1 Baso,2 Mie Ayam, 3 Nasi Goreng
+*/
+switch (menu) {
+    case 1 :
+    console.log("baso")
+    break;
+
+    case 2 :
+        console.log("mie ayam")
+        break;
+
+        case 3:
+            console.log("nasi goreng")
+}
