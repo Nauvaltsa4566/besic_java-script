04e766509915993321fcf68524275ca8272a76f4
Naufal Fadhilla <fadhillanaufal6@gmail.com>
2022-10-11 21:25:34 -0700

    Latihan tanggal 12-10-22

---
+++ latihan18.1.js
@@ -0,0 +1,30 @@
+var color =5;
+/*
+1 like 2 drak 3 noktur 4 terminal 5 indigo
+*/
+
+/*switch(color){
+    case 1:
+        console.log("like")
+        break;
+        case 2 :
+            console.log("drak")
+            break;
+        case 3 :
+            console.log("noktur")
+            break;
+        case 4 :
+            console.log("terminal")
+            break;
+        case 5 :
+            console.log("indigo")
+            break;
+}
+    */
+   // your code here
+   {
+   console.log(color == 1 ? "light":
+   (color == 2 ? "drak" :
+   (color == 3 ? "noktur":
+   color == 4 ? "terminal" : "indigo")))
+   }
