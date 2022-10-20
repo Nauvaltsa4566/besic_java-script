6abc645d4dbccb774f9e066059a476242128580c
Naufal Fadhilla <fadhillanaufal6@gmail.com>
2022-10-19 23:45:22 -0700

    tgl 20-10-2022_function

---
+++ function_parameters.js
@@ -0,0 +1,17 @@
+//function parameters
+function name(nauval, naila, naura){
+    //some code
+}
+//using parameter
+function sayHello(name){
+    alert("Hi,"+name);
+}
+sayHello("nauval");
+//functionparameters
+function sayHello(name){
+    console.log("Hi," + name);
+    //harusnya menggunakan alert,tetapi jika menggunakan alert tidak bisa di run menggunakan node js
+}
+sayHello("nauval");
+sayHello("naila");
+sayHello("naura");
