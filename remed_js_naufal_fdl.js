3fe21ba89dddb1822e0f71e0e2d7e9186b1aab23
Naufal Fadhilla <fadhillanaufal6@gmail.com>
2022-10-26 23:50:01 -0700

    tgl 27-10-22 remed java script

---
+++ remed_js_naufal_fdl.js
@@ -0,0 +1,91 @@
+// assiment adalah untuk mengisi nilai satu variable contoh nya seperti di bawah ini
+/* operator 
+1.=
+2.+=
+3.-=
+4.*=
+5./=
+6.%=
+7.++
+8.--
+*/
+console.log("assigment")
+var j=2;
+j+=1;
+console.log(j)
+console.log("\n")
+
+// comparison adalah untuk membandingkan satu variable 
+/*1.==
+2.===
+3.!=
+4.!==
+5.>
+6.<
+7.>=
+8.<= 
+*/
+console.log("comparison")
+var d=7;
+console.log(d==8)
+console.log("\n")
+
+//if_else_switch
+var i=76;
+var e=77;
+var d=2;
+/* 
+1 nasi padang, 2 martabak,
+*/
+console.log("for")
+if(e<=79){
+    console.log("benar")
+
+}
+console.log("\n")
+
+console.log("if else")
+if(i>=80){
+    console.log("benar")
+
+}else{
+    console.log("salah")
+}
+console.log("\n")
+
+console.log("switch case")
+switch(d){
+    case 1:
+        console.log("nasi padang")
+        break;
+    default:
+    console.log("martabak")
+
+}
+console.log("\n")
+
+// for itu buat bikin perulangan
+console.log("bilangan for")
+var i=0;
+for(i=1;i<=2;i++){
+    console.log(i)
+}
+console.log("\n")
+
+// while itu untuk mengulang sesuatu proses yang belum tahu jumlah nya 
+console.log("bilangan while")
+var c=7;
+while(c<=11){
+console.log(c)
+c++
+}
+console.log("\n")
+
+// do while bakal ngelakui perulangan sebanyak 1 kal
+console.log("bilangan do while")
+var i=1
+do{
+    console.log(i)
+    i++
+}
+while(i<=3)
